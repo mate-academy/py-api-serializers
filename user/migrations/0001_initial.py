b8d058672bd44a4ce5f320e92d068a7e83067db6
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-05-11 16:23:54 +0300

    Solution

--- user/migrations/0001_initial.py
+++ user/migrations/0001_initial.py
@@ -72,14 +72,17 @@ class Migration(migrations.Migration):
                 (
                     "email",
                     models.EmailField(
-                        blank=True, max_length=254, verbose_name="email address"
+                        blank=True,
+                        max_length=254,
+                        verbose_name="email address"
                     ),
                 ),
                 (
                     "is_staff",
                     models.BooleanField(
                         default=False,
-                        help_text="Designates whether the user can log into this admin site.",
+                        help_text="Designates whether "
+                                  "the user can log into this admin site.",
                         verbose_name="staff status",
                     ),
                 ),
@@ -87,21 +90,29 @@ class Migration(migrations.Migration):
                     "is_active",
                     models.BooleanField(
                         default=True,
-                        help_text="Designates whether this user should be treated as active. Unselect this instead of deleting accounts.",
+                        help_text="Designates whether this "
+                                  "user should be treated as active. "
+                                  "Unselect this instead of deleting "
+                                  "accounts.",
                         verbose_name="active",
                     ),
                 ),
                 (
                     "date_joined",
                     models.DateTimeField(
-                        default=django.utils.timezone.now, verbose_name="date joined"
+                        default=django.utils.timezone.now,
+                        verbose_name="date joined"
                     ),
                 ),
                 (
                     "groups",
                     models.ManyToManyField(
                         blank=True,
-                        help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
+                        help_text="The groups this user "
+                                  "belongs to. "
+                                  "A user will get all "
+                                  "permissions granted to each of "
+                                  "their groups.",
                         related_name="user_set",
                         related_query_name="user",
                         to="auth.group",
