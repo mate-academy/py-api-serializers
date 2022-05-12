e703d61e4c46ccd3f5af55d0a77dd970a661bfb5
Nazar-kob <kobrinnazar05@mail.com>
2022-05-12 14:08:33 +0300

    delete dtb

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "debug_toolbar",
     "rest_framework",
     "cinema",
     "user",
@@ -45,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -85,9 +83,6 @@ DATABASES = {
     }
 }
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
