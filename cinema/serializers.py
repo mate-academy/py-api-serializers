4a98bae18d6771a5d41ff4ca315e771e79d70a89
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-07-14 15:12:23 +0300

    Solutions

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,63 @@
-# write serializers here
+from rest_framework import serializers
+
+from cinema.models import Movie, Genre, Actor, CinemaHall, MovieSession
+
+
+class GenreSerialiser(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+
+class ActorSerialiser(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("first_name", "last_name", "full_name")
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ("id", "title", "description", "duration", "genres", "actors")
+
+
+class MovieListSerializer(MovieSerializer):
+    genres = serializers.StringRelatedField(many=True)
+    actors = serializers.StringRelatedField(many=True)
+
+
+class MovieDetailSerialiser(MovieSerializer):
+    actors = ActorSerialiser(many=True, read_only=True)
+    genres = GenreSerialiser(many=True, read_only=True)
+
+
+class CinemaHallSerialiser(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = ("name", "rows", "seats_in_row", "capacity")
+
+
+class MovieSessionSerialiser(serializers.ModelSerializer):
+    class Meta:
+        model = MovieSession
+        fields = "__all__"
+
+
+class MovieSessionListSerializer(MovieSessionSerialiser):
+    movie_title = serializers.CharField(source="movie.title", read_only=True)
+    cinema_hall_name = serializers.CharField(source="cinema_hall.name",
+                                             read_only=True)
+    cinema_hall_capacity = serializers.IntegerField(
+        source="cinema_hall.capacity",
+        read_only=True
+    )
+
+    class Meta:
+        model = MovieSession
+        fields = ("id", "show_time", "movie_title",
+                  "cinema_hall_name", "cinema_hall_capacity")
+
+
+class MovieSessionDetailSerialiser(MovieSessionSerialiser):
+    movie = MovieListSerializer(read_only=True)
+    cinema_hall = CinemaHallSerialiser(read_only=True)
--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,53 @@
-# write views here
+from rest_framework import viewsets
+
+from cinema.models import Movie, Actor, CinemaHall, Genre, MovieSession
+from cinema.serializers import MovieDetailSerialiser, ActorSerialiser, \
+    CinemaHallSerialiser, GenreSerialiser, MovieSessionSerialiser, \
+    MovieListSerializer, MovieSerializer, MovieSessionListSerializer, \
+    MovieSessionDetailSerialiser
+
+
+class MoviesViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all().prefetch_related("genres", "actors")
+    serializer_class = MovieSerializer
+
+    def get_serializer_class(self):
+
+        if self.action == "list":
+            return MovieListSerializer
+
+        if self.action == "retrieve":
+            return MovieDetailSerialiser
+
+        return MovieSerializer
+
+
+class ActorsViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerialiser
+
+
+class CinemaHallsViewSet(viewsets.ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerialiser
+
+
+class GenresViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerialiser
+
+
+class MovieSessionsViewSet(viewsets.ModelViewSet):
+    queryset = MovieSession.objects.all().select_related("movie",
+                                                         "cinema_hall")
+    serializer_class = MovieSessionSerialiser
+
+    def get_serializer_class(self):
+
+        if self.action == "list":
+            return MovieSessionListSerializer
+
+        if self.action == "retrieve":
+            return MovieSessionDetailSerialiser
+
+        return MovieSessionSerialiser
