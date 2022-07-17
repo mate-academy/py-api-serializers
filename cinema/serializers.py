2e90543b259bca829f1e977e036dabd84d9eff6a
Artem Toporchan <gydratvar@gmail.com>
2022-07-17 15:46:00 +0300

    Fixed

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -14,7 +14,7 @@ class ActorSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Actor
-        fields = "__all__"
+        fields = ("id", "first_name", "last_name", "full_name")
 
 
 class CinemaHallSerializer(serializers.ModelSerializer):
