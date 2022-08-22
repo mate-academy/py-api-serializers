94d7ed7631938146c49bed713575cea1177bbb96
polinatytarenko <p.tytarenko.20@gmail.com>
2022-08-22 11:44:30 +0300

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,52 @@
-# write views here
+from rest_framework.response import Response
+from rest_framework import status, generics, mixins, viewsets
+
+from rest_framework.views import APIView
+
+from cinema.models import Movie, Genre, Actor, CinemaHall, MovieSession
+from cinema.serializers import (
+    MovieSerializer, GenreSerializer, ActorSerializer, CinemaHallSerializer,
+    MovieSessionSerializer, MovieSessionDetailSerializer,
+    MovieDetailSerializer, MovieListSerializer, MovieSessionListSerializer,
+)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class CinemaHallViewSet(viewsets.ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.prefetch_related("actors", "genres")
+    serializer_class = MovieSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieListSerializer
+        if self.action == "retrieve":
+            return MovieDetailSerializer
+
+        return MovieSerializer
+
+
+class MovieSessionViewSet(viewsets.ModelViewSet):
+    queryset = MovieSession.objects.select_related("movie", "cinema_hall")
+    serializer_class = MovieSessionSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieSessionListSerializer
+        if self.action == "retrieve":
+            return MovieSessionDetailSerializer
+
+        return MovieSessionSerializer
