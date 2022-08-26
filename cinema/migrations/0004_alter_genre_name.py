d05f67e2fcedaeff65a82f6a9b341e18c4110eef
Marko <marko@MacBook-Pro-Marko.local>
2022-08-27 01:07:00 +0300

    solution

--- cinema/migrations/0004_alter_genre_name.py
+++ cinema/migrations/0004_alter_genre_name.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cinema', '0003_movie_duration'),
+        ("cinema", "0003_movie_duration"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='genre',
-            name='name',
+            model_name="genre",
+            name="name",
             field=models.CharField(max_length=255, unique=True),
         ),
     ]
