cc7b41eeae5f60cebfa5df5f51186d84446834c8
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-11-18 18:23:07 +0200

    Fix debug toolbar now

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "rest_framework",
     "cinema",
     "user",
@@ -132,3 +133,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
