f177ef234f9b95427781febf44a8769024ff6b0e
Sergii Franchuk <Franchuksergei@gmail.com>
2022-11-20 19:40:05 +0200

    setup

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,36 @@
-# write serializers here
+from rest_framework import serializers
+
+from cinema.models import (
+    CinemaHall, Genre, Actor, Movie, MovieSession
+)
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CinemaHall
+        fields = "__all__"
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = "__all__"
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = "__all__"
+
+
+class MovieSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovieSession
+        fields = "__all__"
