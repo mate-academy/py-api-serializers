a557d35eeb568b5436f56c409d963c650dba86cb
Khrystyna Skrypko <christina.skrypko@gmail.com>
2022-11-21 19:12:08 +0200

    add type annotations

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,4 @@
+from django.db.models import QuerySet
 from rest_framework import viewsets
 
 from cinema.models import (
@@ -30,7 +31,7 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
         queryset = self.queryset
 
         if self.action in ("list", "retrieve"):
@@ -52,7 +53,7 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.all()
     serializer_class = MovieSessionSerializer
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
         queryset = self.queryset
 
         if self.action in ("list", "retrieve"):
