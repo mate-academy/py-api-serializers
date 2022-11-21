e93702ee569f03e3d90a3c5e7f017419855c635f
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-11-21 12:47:54 +0200

    Fix flake8

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -48,8 +48,14 @@ class MovieSessionSerializer(serializers.ModelSerializer):
 
 
 class MovieSessionListSerializer(MovieSerializer):
-    movie_title = serializers.CharField(source="movie.title", read_only=True)
-    cinema_hall_name = serializers.CharField(source="cinema_hall.name", read_only=True)
+    movie_title = serializers.CharField(
+        source="movie.title",
+        read_only=True
+    )
+    cinema_hall_name = serializers.CharField(
+        source="cinema_hall.name",
+        read_only=True
+    )
     cinema_hall_capacity = serializers.IntegerField(
         source="cinema_hall.capacity", read_only=True
     )
