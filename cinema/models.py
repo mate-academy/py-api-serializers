3f0d27c55e496f3e9df01e68f9297115ce0fdd54
Dmytro Skrypka <2346677tyu@gmail.com>
2022-11-22 12:54:32 +0200

    Added property decorator for full_name

--- cinema/models.py
+++ cinema/models.py
@@ -27,6 +27,7 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
+    @property
     def full_name(self) -> str:
         return f"{self.first_name} {self.last_name}"
 
