220631d72087c31fabfe4ff627b0705fff185867
Stas Okhrym <s.okhrym@gmail.com>
2022-11-22 18:57:27 +0200

    Removed redundant __str__ method in Actor model

--- cinema/models.py
+++ cinema/models.py
@@ -27,9 +27,6 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    def __str__(self) -> str:
-        return self.first_name + " " + self.last_name
-
     @property
     def full_name(self) -> str:
         return f"{self.first_name} {self.last_name}"
