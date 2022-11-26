5dd3b5d9bf94e9bfc36b173afca94cceee91088a
unknown <vladearth7@gmail.com>
2022-11-26 04:50:18 +0200

    'addtypeanotations'

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, QuerySet
 from rest_framework import viewsets
 from cinema.models import (
     Genre,
@@ -39,14 +39,14 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> type(serializer_class):
         if self.action == "retrieve":
             return MovieDetailSerializer
         if self.action == "list":
             return MovieListSerializer
         return MovieSerializer
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
         queryset = self.queryset
         if self.action == "list":
             queryset = queryset.prefetch_related(Prefetch("actors"),
@@ -58,14 +58,14 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.all()
     serializer_class = MovieSessionSerializer
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> type(serializer_class):
         if self.action == "retrieve":
             return MovieSessionDetailSerializer
         if self.action == "list":
             return MovieSessionListSerializer
         return MovieSessionSerializer
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
         queryset = self.queryset
         if self.action == "list":
             queryset = queryset.prefetch_related(Prefetch("movie"),
