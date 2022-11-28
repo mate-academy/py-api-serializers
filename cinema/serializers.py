18ac6a8b6c05686a52d9760c3e5fa27b1fc79f77
Dima Ovcharenko <kukuku381@gmail.com>
2022-11-28 15:05:29 +0200

    Fixed

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -47,10 +47,12 @@ class MovieDetailSerializer(MovieSerializer):
 
 class MovieSessionListSerializer(MovieSessionSerializer):
     movie_title = serializers.CharField(source="movie.title", read_only=True)
-    cinema_hall_name = serializers.CharField(source="cinema_hall.name",
-                                             read_only=True)
+    cinema_hall_name = serializers.CharField(
+        source="cinema_hall.name", read_only=True
+    )
     cinema_hall_capacity = serializers.IntegerField(
-        source="cinema_hall.capacity", read_only=True)
+        source="cinema_hall.capacity", read_only=True
+    )
 
     class Meta:
         model = MovieSession
