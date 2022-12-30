3e56c3521972d25508930ac183d4fd1e330788a9
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-12-30 15:59:19 +0200

    Refactoring serializers.py

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -34,12 +34,20 @@ class ActorSerializer(serializers.ModelSerializer):
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ("id", "title", "description", "duration", "genres", "actors")
+        fields = (
+            "id",
+            "title",
+            "description",
+            "duration",
+            "genres",
+            "actors"
+        )
 
 
 class MovieListSerializer(MovieSerializer):
     genres = serializers.SlugRelatedField(
-        many=True, read_only=True, slug_field="name")
+        many=True, read_only=True, slug_field="name"
+    )
     actors = serializers.StringRelatedField(many=True, read_only=True)
 
 
@@ -57,14 +65,19 @@ class MovieSessionSerializer(serializers.ModelSerializer):
 class MovieSessionListSerializer(MovieSessionSerializer):
     movie_title = serializers.CharField(source="movie.title", read_only=True)
     cinema_hall_name = serializers.CharField(
-        source="cinema_hall.name", read_only=True)
+        source="cinema_hall.name",
+        read_only=True
+    )
     cinema_hall_capacity = serializers.IntegerField(
-        source="cinema_hall.capacity", read_only=True)
+        source="cinema_hall.capacity",
+        read_only=True
+    )
 
     class Meta:
         model = MovieSession
         fields = (
-            "id", "show_time",
+            "id",
+            "show_time",
             "movie_title",
             "cinema_hall_name",
             "cinema_hall_capacity"
