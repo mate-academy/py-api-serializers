d6fcc3229c01375336d2eac6760f00651dee3ab7
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2023-01-02 18:31:43 -0400

    Solution for serializors

--- cinema/models.py
+++ cinema/models.py
@@ -19,6 +19,9 @@ class CinemaHall(models.Model):
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    class Meta:
+        verbose_name_plural = "genres"
+
     def __str__(self):
         return self.name
 
@@ -26,6 +29,10 @@ class Genre(models.Model):
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
 
     def __str__(self):
         return self.first_name + " " + self.last_name
