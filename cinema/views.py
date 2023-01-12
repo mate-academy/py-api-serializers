4d54411f3df02e884ac2201606d2720e4787834a
Timur <timur.hike@gmail.com>
2023-01-12 14:39:19 +0200

    solution

--- cinema/views.py
+++ cinema/views.py
@@ -40,7 +40,7 @@ class ActorViewSet(viewsets.ModelViewSet):
 
 
 class MovieViewSet(viewsets.ModelViewSet):
-    queryset = Movie.objects.all().prefetch_related("genres", "actors")
+    queryset = Movie.objects.prefetch_related("genres", "actors")
     serializer_class = MovieSerializer
 
     def get_serializer_class(self):
