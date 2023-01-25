7afba0ad064e02c209c0b2a413288a7bc3719ad2
IvanGLS <gladiolus0707@gmail.com>
2023-01-25 16:21:09 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -54,7 +54,7 @@ class MovieViewSet(viewsets.ModelViewSet):
         if self.action == "retrieve":
             return MovieDetailSerializer
 
-        return MovieSerializer
+        return self.serializer_class
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
@@ -75,4 +75,4 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         if self.action == "retrieve":
             return MovieSessionDetailSerializer
 
-        return MovieSessionSerializer
+        return self.serializer_class
