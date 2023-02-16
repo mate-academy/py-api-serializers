f01c473f47cdc1af1d8017e0ec239800184cf8e9
Diana <daria.blessed18@gmail.com>
2023-02-16 16:56:34 +0200

    V1

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "cinema_service/",
+        include("cinema.urls", namespace="cinema-service")
+    ),
 ]
