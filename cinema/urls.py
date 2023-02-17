18ef0a716677e4df8a632f17ae5da6d90b87a430
vani@linux <winfkill@gmail.com>
2023-02-17 14:58:16 +0200

    2 fails

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,18 @@
 # write urls here
+from django.urls import path, include
+from rest_framework import routers
+
+from cinema.views import GenreViewSet, ActorViewSet, CinemaHallViewSet, MovieViewSet, MovieSessionViewSet
+
+router = routers.SimpleRouter()
+router.register('genres',GenreViewSet)
+router.register('actors',ActorViewSet)
+router.register('cinema_halls',CinemaHallViewSet)
+router.register('movies',MovieViewSet)
+router.register('movie_sessions',MovieSessionViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,44 @@
 # write views here
+from rest_framework.viewsets import ModelViewSet
+
+from cinema.models import Genre,Actor,CinemaHall,Movie,MovieSession
+from cinema.serializers import GenreListSerializer, ActorListSerializer, CinemaHallListSerializer, MovieListSerializer, \
+    MovieSessionSerializer, MovieDetailSerializer, MovieSessionDetailSerializer, ActorDetailSerializer
+
+
+class GenreViewSet(ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreListSerializer
+
+class ActorViewSet(ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorListSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ActorListSerializer
+        return ActorDetailSerializer
+
+
+class CinemaHallViewSet(ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallListSerializer
+
+
+class MovieViewSet(ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieListSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieListSerializer
+        return MovieDetailSerializer
+
+class MovieSessionViewSet(ModelViewSet):
+    queryset = MovieSession.objects.all()
+    serializer_class = MovieSessionSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieSessionSerializer
+        return MovieSessionDetailSerializer
