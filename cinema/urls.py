181ab38b6515562aa46ff8f46db993c7940ad985
Paul Starodub <amorallex@gmail.com>
2023-02-24 15:22:24 +0400

    Updated serializers.py & views.py & urls.py for genre

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,3 +1,13 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework import routers
+
+from cinema.views import GenreViewSet
+
+router = routers.DefaultRouter()
+router.register("genres", GenreViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
 
 app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,9 @@
-# write views here
+from rest_framework import viewsets
+
+from cinema.models import Genre
+from cinema.serializers import GenreSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
