05b482ba3245a0cc9bcd8481622c717bc24d8023
Paul Starodub <amorallex@gmail.com>
2023-02-24 15:13:34 +0400

    Updated settings.py & urls.py with django-debug-toolbar

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/cinema/", include("cinema.urls", namespace="cinema"))
+    path("api/cinema/", include("cinema.urls", namespace="cinema")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
