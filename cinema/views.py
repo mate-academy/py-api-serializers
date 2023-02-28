39248021fc31ae4bf4d0f1b29d2e00de64626a14
Denys Tereshchuk <terrya@ukr.net>
2023-02-28 20:56:38 +0400

    Updated annotation

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, serializers
+from rest_framework import viewsets
+from rest_framework.serializers import Serializer
 
 from cinema.models import CinemaHall, Genre, Actor, Movie, MovieSession
 from cinema.serializers import (
@@ -18,7 +19,7 @@ from cinema.serializers import (
 class CinemaHallViewSet(viewsets.ModelViewSet):
     queryset = CinemaHall.objects.all()
 
-    def get_serializer_class(self) -> serializers:
+    def get_serializer_class(self) -> Serializer:
         if self.action in ("list", "retrieve"):
             return CinemaHallListSerializer
         return CinemaHallSerializer
@@ -37,13 +38,13 @@ class ActorViewSet(viewsets.ModelViewSet):
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
 
-    def get_queryset(self) -> queryset:
+    def get_queryset(self) -> queryset[Movie]:
         queryset = self.queryset
         if self.action in ("list", "retrieve"):
             return queryset.prefetch_related("genres", "actors")
         return queryset
 
-    def get_serializer_class(self) -> serializers:
+    def get_serializer_class(self) -> Serializer:
         if self.action == "list":
             return MovieListSerializer
         if self.action == "retrieve":
@@ -54,13 +55,13 @@ class MovieViewSet(viewsets.ModelViewSet):
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.all()
 
-    def get_queryset(self) -> queryset:
+    def get_queryset(self) -> queryset[MovieSession]:
         queryset = self.queryset
         if self.action in ("list", "retrieve"):
             return queryset.select_related("movie", "cinema_hall")
         return queryset
 
-    def get_serializer_class(self) -> serializers:
+    def get_serializer_class(self) -> Serializer:
         if self.action == "list":
             return MovieSessionListSerializer
         if self.action == "retrieve":
