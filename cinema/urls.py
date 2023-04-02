12a0e4521a638cb61fdb1ac09efe9712dc0cf934
bohdanlysuchenko <lysachenko111@ukr.net>
2023-04-02 15:11:45 +0300

    Solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,21 @@
-# write urls here
+from rest_framework import routers
+from django.urls import path, include
+from cinema.views import (
+    GenreViewSet,
+    ActorViewSet,
+    CinemaHallViewSet,
+    MovieViewSet,
+    MovieSessionViewSet,
+)
+
+
+router = routers.DefaultRouter()
+router.register(prefix="genres", viewset=GenreViewSet)
+router.register(prefix="actors", viewset=ActorViewSet)
+router.register(prefix="cinema_halls", viewset=CinemaHallViewSet)
+router.register(prefix="movies", viewset=MovieViewSet)
+router.register(prefix="movie_sessions", viewset=MovieSessionViewSet)
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "cinema"
--- user/migrations/0001_initial.py
+++ user/migrations/0001_initial.py
@@ -7,7 +7,6 @@ import django.utils.timezone
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
@@ -27,7 +26,10 @@ class Migration(migrations.Migration):
                         verbose_name="ID",
                     ),
                 ),
-                ("password", models.CharField(max_length=128, verbose_name="password")),
+                (
+                    "password",
+                    models.CharField(max_length=128, verbose_name="password"),
+                ),
                 (
                     "last_login",
                     models.DateTimeField(
@@ -72,7 +74,9 @@ class Migration(migrations.Migration):
                 (
                     "email",
                     models.EmailField(
-                        blank=True, max_length=254, verbose_name="email address"
+                        blank=True,
+                        max_length=254,
+                        verbose_name="email address",
                     ),
                 ),
                 (
@@ -94,7 +98,8 @@ class Migration(migrations.Migration):
                 (
                     "date_joined",
                     models.DateTimeField(
-                        default=django.utils.timezone.now, verbose_name="date joined"
+                        default=django.utils.timezone.now,
+                        verbose_name="date joined",
                     ),
                 ),
                 (
