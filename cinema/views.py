2c159a19959a48da164d7647c1af4ee6940cbaeb
Sergii Machulin <machulinsv@gmail.com>
2023-04-05 19:24:33 +0300

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,74 @@
-# write views here
+from typing import Type, Union
+
+from rest_framework import viewsets
+
+from cinema.models import (
+    Movie,
+    Genre,
+    Actor,
+    CinemaHall,
+    MovieSession,
+)
+
+from cinema.serializers import (
+    MovieSerializer,
+    GenreSerializer,
+    ActorSerializer,
+    CinemaHallSerializer,
+    MovieSessionSerializer,
+    MovieDetailSerializer,
+    MovieListSerializer,
+    MovieSessionDetailSerializer,
+    MovieSessionListSerializer,
+)
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.prefetch_related("actors", "genres")
+    serializer_class = MovieSerializer
+
+    def get_serializer_class(self) -> Union[
+        Type[MovieListSerializer],
+        Type[MovieDetailSerializer],
+        Type[MovieSerializer]
+    ]:
+        if self.action == "list":
+            return MovieListSerializer
+
+        if self.action == "retrieve":
+            return MovieDetailSerializer
+
+        return MovieSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class CinemaHallViewSet(viewsets.ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
+
+
+class MovieSessionViewSet(viewsets.ModelViewSet):
+    queryset = MovieSession.objects.select_related("movie", "cinema_hall")
+    serializer_class = MovieSessionSerializer
+
+    def get_serializer_class(self) -> Union[
+        Type[MovieSessionListSerializer],
+        Type[MovieSessionDetailSerializer],
+        Type[MovieSessionSerializer]
+    ]:
+        if self.action == "list":
+            return MovieSessionListSerializer
+
+        if self.action == "retrieve":
+            return MovieSessionDetailSerializer
+
+        return MovieSessionSerializer
