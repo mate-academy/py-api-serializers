bd82915d5e56f856e18d713fce407865fff121d2
Paul Savchuk <nekrjkeee@gmail.com>
2023-04-05 21:35:06 +0300

    add skeleton serializers

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,31 @@
-# write serializers here
+from rest_framework import serializers
+from .models import Genre, Movie, Actor, MovieSession
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = "__all__"
+
+
+class CinemaHall(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = "__all__"
+
+
+class MovieSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovieSession
+        fields = "__all__"
