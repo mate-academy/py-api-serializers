d4708888de5fd37359a6dc990ab8ff6ab70eb710
Maksym Nikityuk <maksym71417@gmail.com>
2023-04-06 14:39:58 +0300

    Serializers Solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,53 @@
-# write views here
+from rest_framework import viewsets
+
+from cinema.models import Movie, Actor, CinemaHall, MovieSession, Genre
+from cinema.serializers import (MovieSerializer,
+                                ActorSerializer,
+                                CinemaHallSerializer,
+                                MovieSessionSerializer,
+                                GenreSerializer,
+                                MovieListSerializer,
+                                MovieDetailSerializer,
+                                MovieSessionListSerializer,
+                                MovieSessionDetailSerializer)
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.prefetch_related("actors", "genres")
+    serializer_class = MovieSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieListSerializer
+        if self.action == "retrieve":
+            return MovieDetailSerializer
+
+        return MovieSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class CinemaHallViewSet(viewsets.ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
+
+
+class MovieSessionViewSet(viewsets.ModelViewSet):
+    queryset = MovieSession.objects.prefetch_related("cinema_hall", "movie")
+    serializer_class = MovieSessionSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieSessionListSerializer
+        if self.action == "retrieve":
+            return MovieSessionDetailSerializer
+
+        return MovieSessionSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
