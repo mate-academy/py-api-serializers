b0ace673b5c8454a91bd37846edee654212e18f2
usrofgh <mailforgimail@gmail.com>
2023-04-06 18:56:38 +0300

    update

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("__debug__/", include("debug_toolbar.urls")),
     path("admin/", admin.site.urls),
     path("api/cinema/", include("cinema.urls")),
 ]
