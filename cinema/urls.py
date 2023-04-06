f4ad7648f5bacfaa2cc09b724ac9e4e6b2fa8557
Danylo <danildacenko@gmail.com>
2023-04-07 01:29:01 +0300

    Solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,24 @@
-# write urls here
+from rest_framework import routers
+
+from cinema.views import (
+    GenreViewSet,
+    ActorViewSet,
+    CinemaHallViewSet,
+    MovieViewSet,
+    MovieSessionViewSet
+)
+
+router = routers.DefaultRouter()
+router.register("actors", ActorViewSet, basename="actor")
+router.register("genres", GenreViewSet, basename="genre")
+router.register("cinema_halls", CinemaHallViewSet, basename="cinema-hall")
+router.register("movies", MovieViewSet, basename="movie")
+router.register(
+    "movie_sessions",
+    MovieSessionViewSet,
+    basename="movie-session"
+)
+
+urlpatterns = router.urls
+
+app_name = "cinema"
