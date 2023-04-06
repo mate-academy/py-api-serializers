5e3a528eb7f436614041bbe5766e07b8932b4d9e
Yevhen Kaskov <kaskov.e@gmail.com>
2023-04-06 16:05:49 +0300

    changed return in Actor's str

--- cinema/models.py
+++ cinema/models.py
@@ -27,13 +27,12 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.first_name + " " + self.last_name
-
     @property
     def full_name(self):
         return self.first_name + " " + self.last_name
 
+    def __str__(self):
+        return self.full_name
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
--- cinema/views.py
+++ cinema/views.py
@@ -52,7 +52,7 @@ class CinemaHallViewSet(viewsets.ModelViewSet):
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
-    queryset = MovieSession.objects.all()
+    queryset = MovieSession.objects.select_related("movie", "cinema_hall")
 
     def get_serializer_class(self):
         if self.action == "list":
