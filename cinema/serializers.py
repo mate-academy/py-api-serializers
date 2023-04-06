9e4592da8ba8924ca482785c27fd7f974edc807e
Paul Savchuk <nekrjkeee@gmail.com>
2023-04-06 07:39:19 +0300

    add MoveViewSet related to readme info

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Genre, Movie, Actor, MovieSession
+from .models import Genre, Movie, Actor, MovieSession, CinemaHall
 
 
 class GenreSerializer(serializers.ModelSerializer):
@@ -16,16 +16,33 @@ class ActorSerializer(serializers.ModelSerializer):
 
 class CinemaHall(serializers.ModelSerializer):
     class Meta:
-        model = Movie
+        model = CinemaHall
+        fields = "__all__"
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    genres = GenreSerializer(many=True, read_only=False)  # to return data from relate
+    actors = ActorSerializer(many=True, read_only=False)
+
     class Meta:
         model = Movie
         fields = "__all__"
 
 
+class MovieListSerializer(MovieSerializer):
+    genres = serializers.StringRelatedField(
+        many=True,
+        read_only=True
+    )  # to return data from relate
+    actors = serializers.StringRelatedField(
+        many=True,
+        read_only=True
+    )
+
+
 class MovieSessionSerializer(serializers.ModelSerializer):
+    capacity = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = MovieSession
         fields = "__all__"
--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,16 @@
-# write views here
+from rest_framework import viewsets
+
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer, MovieListSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.prefetch_related("actors", "genres")
+    serializer_class = MovieSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieListSerializer
+
+        return MovieSerializer
+
