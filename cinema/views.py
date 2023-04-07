c212a5bff289cea01366b44f744efc14795b6dba
Roman Senyshyn <senjaskorp@gmail.com>
2023-04-07 19:47:52 +0300

    Fixed pep8 and changed serializer to self.serializer

--- cinema/views.py
+++ cinema/views.py
@@ -51,7 +51,7 @@ class MovieViewSet(viewsets.ModelViewSet):
         if self.action == "retrieve":
             return MovieDetailSerializer
 
-        return MovieSerializer
+        return self.serializer_class
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
@@ -76,4 +76,4 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         if self.action == "retrieve":
             return MovieSessionDetailSerializer
 
-        return MovieSessionSerializer
+        return self.serializer_class
