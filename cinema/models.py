3da2eb0f1e5b6d00ed760431e980f6ca0f25ae5a
Mykhailo Vovchok <mivvaw@gmail.com>
2023-04-07 09:23:19 +0300

    solution

--- cinema/models.py
+++ cinema/models.py
@@ -28,15 +28,15 @@ class Actor(models.Model):
     last_name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return f"{self.first_name} {self.last_name}"
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
-    genres = models.ManyToManyField(Genre)
-    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(to=Genre)
+    actors = models.ManyToManyField(to=Actor)
 
     class Meta:
         ordering = ["title"]
@@ -54,7 +54,7 @@ class MovieSession(models.Model):
         ordering = ["-show_time"]
 
     def __str__(self):
-        return self.movie.title + " " + str(self.show_time)
+        return f"{self.movie.title} {str(self.show_time)}"
 
 
 class Order(models.Model):
