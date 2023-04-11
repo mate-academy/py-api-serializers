bacf355dbe8aef74c248d42fcef6e117e933d3a7
Viktoria <rybenchukv@gmail.com>
2023-04-11 17:03:17 +0200

    Fixed blank line

--- cinema/views.py
+++ cinema/views.py
@@ -49,7 +49,7 @@ class MovieViewSet(ModelViewSet):
             return MovieListSerializer
         if self.action == "retrieve":
             return MovieDetailSerializer
-        return MovieSerializer
+        return self.serializer_class
 
 
 class MovieSessionViewSet(ModelViewSet):
@@ -64,4 +64,4 @@ class MovieSessionViewSet(ModelViewSet):
         elif self.action == "retrieve":
             return MovieSessionDetailSerializer
 
-        return MovieSessionSerializer
+        return self.serializer_class
