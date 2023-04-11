7d933552fbee931bebf9c0cb0810615626da4ada
Anatolii Pozniak <a13101990a@gmail.com>
2023-04-11 21:49:47 +0300

    change select related

--- cinema/views.py
+++ cinema/views.py
@@ -38,7 +38,7 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = self.queryset
         if self.action in ("list", "retrieve"):
-            queryset = queryset.prefetch_related("movie", "cinema_hall")
+            queryset = queryset.select_related("movie", "cinema_hall")
         return queryset
 
     def get_serializer_class(
