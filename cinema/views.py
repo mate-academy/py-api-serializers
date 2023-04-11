f2e99a68cde91108ba1b82d7b197bc402d185b24
Serhii Melnyk <serg.melnyk1998@gmail.com>
2023-04-11 17:40:42 +0300

    'Solution'

--- cinema/views.py
+++ cinema/views.py
@@ -52,7 +52,10 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         if self.action in ("list", "retrieve"):
-            self.queryset = self.queryset.select_related("movie", "cinema_hall")
+            self.queryset = self.queryset.select_related(
+                "movie",
+                "cinema_hall"
+            )
         return self.queryset
 
     def get_serializer_class(self):
