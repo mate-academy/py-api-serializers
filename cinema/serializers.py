f770d87ea79df5985f3cdcf3bedf27fb2616b409
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-04-28 09:02:56 +0300

    Implement MovieListSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,33 @@
-# write serializers here
+from rest_framework import serializers
+
+from cinema.models import Movie, Genre, Actor
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("id", "name")
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name")
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = (
+            "id",
+            "title",
+            "description",
+            "duration",
+            "genres",
+            "actors"
+        )
+
+
+class MovieListSerializer(MovieSerializer):
+    genres = GenreSerializer(read_only=True, many=True)
+    actors = ActorSerializer(read_only=True, many=True)
--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,15 @@
-# write views here
+from rest_framework import viewsets
+
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer, MovieListSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieListSerializer
+
+        return MovieSerializer
