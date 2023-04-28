96ab87fbd8aa93eb0ade4030af1bfbbf801b918a
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-04-28 09:19:56 +0300

    Implement get_queryset() in MovieViewSet

--- cinema/views.py
+++ cinema/views.py
@@ -18,6 +18,14 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.action == "list":
+            queryset = queryset.prefetch_related("genres", "actors")
+
+        return queryset
+
     def get_serializer_class(self):
         if self.action == "list":
             return MovieListSerializer
