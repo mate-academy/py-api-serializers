12bf91f1fb6913c35cfc49942eedee2a2aeac7e8
Andriy <andriykrav@gmail.com>
2023-05-07 15:24:50 +0300

    Solution_some tests fails

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,101 @@
-# write serializers here
+from rest_framework import serializers
+from .models import Genre, Actor, CinemaHall, Movie, MovieSession
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
+    class Meta:
+        model = Actor
+        fields = ["id", "first_name", "last_name", "full_name"]
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = ["id", "name", "rows", "seats_in_row", "capacity"]
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    actors = serializers.StringRelatedField(many=True, read_only=True)
+    genres = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="name"
+    )
+
+    class Meta:
+        model = Movie
+        fields = "__all__"
+
+
+class MovieDetailSerializer(serializers.ModelSerializer):
+    actors = serializers.StringRelatedField(many=True, read_only=True)
+    genres = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="name"
+    )
+
+    class Meta:
+        model = Movie
+        fields = "__all__"
+
+
+class MovieSessionSerializer(serializers.ModelSerializer):
+    movie_title = serializers.SerializerMethodField()
+    cinema_hall_name = serializers.SerializerMethodField()
+    cinema_hall_capacity = serializers.SerializerMethodField()
+
+    def get_movie_title(self, obj):
+        return obj.movie.title
+
+    def get_cinema_hall_name(self, obj):
+        return obj.cinema_hall.name
+
+    def get_cinema_hall_capacity(self, obj):
+        return obj.cinema_hall.capacity
+
+    def create(self, validated_data):
+        if "cinema_hall_id" in validated_data:
+            cinema_hall_id = validated_data.pop("cinema_hall_id")
+        else:
+            raise serializers.ValidationError("cinema_hall_id is required.")
+        try:
+            cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+        except CinemaHall.DoesNotExist:
+            raise serializers.ValidationError("Invalid cinema hall.")
+
+        movie_session = MovieSession.objects.create(
+            cinema_hall=cinema_hall, **validated_data
+        )
+
+        return movie_session
+
+    class Meta:
+        model = MovieSession
+        fields = [
+            "id",
+            "show_time",
+            "movie_title",
+            "cinema_hall_name",
+            "cinema_hall_capacity"
+        ]
+
+
+class MovieSessionDetailSerializer(serializers.ModelSerializer):
+    movie = MovieDetailSerializer()
+    cinema_hall = CinemaHallSerializer()
+
+    class Meta:
+        model = MovieSession
+        fields = "__all__"
