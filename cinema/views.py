788b4454a04b2f3ee7f67d50109e0c7f4c3b4563
soobig666 <darkvader.ukr.net@gmail.com>
2023-05-31 12:17:56 +0300

    add views for model, using serializers in serializers.py

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,44 @@
-# write views here
+from rest_framework import viewsets
+from .models import Genre, Actor, CinemaHall, Movie, MovieSession
+from .serializers import (
+    GenreSerializer,
+    ActorSerializer,
+    CinemaHallSerializer,
+    MovieListSerializer,
+    MovieDetailSerializer,
+    MovieSessionListSerializer,
+    MovieSessionDetailSerializer,
+)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class CinemaHallViewSet(viewsets.ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all().prefetch_related('genres', 'actors')
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return MovieListSerializer
+        return MovieDetailSerializer
+
+
+class MovieSessionViewSet(viewsets.ModelViewSet):
+    queryset = MovieSession.objects.all().prefetch_related('movie', 'cinema_hall', 'movie__genres', 'movie__actors')
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return MovieSessionListSerializer
+        return MovieSessionDetailSerializer
