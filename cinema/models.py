64460779141198cf5f4a49b7c95039838bc32d1c
Andy <andy77andy@ukr.net>
2023-07-16 18:40:19 +0300

    'Solution'

--- cinema/models.py
+++ cinema/models.py
@@ -28,11 +28,11 @@ class Actor(models.Model):
     last_name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return f"{self.first_name} {self.last_name}"
 
     @property
     def full_name(self) -> str:
-        return self.first_name + " " + self.last_name
+        return f"{self.first_name} {self.last_name}"
 
 
 class Movie(models.Model):
@@ -58,7 +58,7 @@ class MovieSession(models.Model):
         ordering = ["-show_time"]
 
     def __str__(self):
-        return self.movie.title + " " + str(self.show_time)
+        return f"{self.movie.title}  {str(self.show_time)}"
 
 
 class Order(models.Model):
