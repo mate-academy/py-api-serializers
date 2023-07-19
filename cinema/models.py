5da89b84f85f93302865b2aaeeece427e4ed000c
muhactive <muhactive@gmail.com>
2023-07-19 20:15:42 +0300

    api-serializers

--- cinema/models.py
+++ cinema/models.py
@@ -27,6 +27,9 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
     def __str__(self):
         return self.first_name + " " + self.last_name
 
