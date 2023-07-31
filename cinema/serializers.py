fc75c4ead46ac22d79bf3d5797797dae67d6f432
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-07-31 14:46:27 +0300

    created CinemaHallSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from cinema.models import Genre, Actor
+from cinema.models import Genre, Actor, CinemaHall
 
 
 class GenreSerializer(serializers.ModelSerializer):
@@ -15,3 +15,11 @@ class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
         fields = ("id", "first_name", "last_name", "full_name",)
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CinemaHall
+        fields = ("name", "rows", "seats_in_row", "capacity")
+
