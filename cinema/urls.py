b77f4f48931b9abff1e0c0ebec5cd5b3eaede041
Maksym Bezhevets <begik4@gmail.com>
2023-07-31 22:21:07 +0300

    Changed urlpatterns

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,4 +1,3 @@
-from django.urls import path, include
 from rest_framework import routers
 
 from cinema.views import (
@@ -16,6 +15,6 @@ router.register("cinema_halls", CinemaHallViewSet)
 router.register("movies", MovieViewSet)
 router.register("movie_sessions", MovieSessionViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = router.urls
 
 app_name = "cinema"
