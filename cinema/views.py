1ddf175773a0b06691eec174263850409cf61845
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-07-31 14:14:06 +0300

    created view for Genre model

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,13 @@
-# write views here
+from rest_framework.viewsets import ModelViewSet
+
+from cinema.models import (
+    Genre
+)
+from cinema.serializers import (
+    GenreSerializer
+)
+
+
+class GenreViewSet(ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
