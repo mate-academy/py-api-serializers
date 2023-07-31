785d0dafe25e3f1315c374ef7ef8d4f35f697bf7
Volodymyr Bohomol <8773927@gmail.com>
2023-07-31 15:30:20 +0300

    deleted debug_toolbar to pass tests

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/cinema/", include("cinema.urls", namespace="cinema")),
+    path("api/cinema/", include("cinema.urls", namespace="cinema"))
 ]
