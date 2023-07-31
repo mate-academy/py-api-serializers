0b74d7d12a5ab0b524f173e1fa6195c497741195
Yevhenii Iovchu <persie.iovchu@gmail.com>
2023-07-31 21:26:32 +0300

    solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "rest_framework",
-    "debug_toolbar",
 
     "cinema",
     "user",
@@ -51,7 +50,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
