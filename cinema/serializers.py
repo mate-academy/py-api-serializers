b5a2b668aa485fc0c7bae75cc7afd7614cb4a8de
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-07-31 14:36:15 +0300

    updated ActorSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -14,4 +14,4 @@ class ActorSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Actor
-        fields = "__all__"
+        fields = ("id", "first_name", "last_name", "full_name",)
