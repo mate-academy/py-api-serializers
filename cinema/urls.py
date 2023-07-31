d63d5d14ffd8e72eef75a207ebab4b0881ec3b2f
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-07-31 14:14:35 +0300

    created router for GenreViewSet

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,14 @@
-# write urls here
+from django.urls import path, include
+from rest_framework import routers
+
+from cinema.views import GenreViewSet
+
+genre_router = routers.DefaultRouter()
+genre_router.register("genres", GenreViewSet, basename="genres")
+
+
+urlpatterns = [
+    path("", include(genre_router.urls))
+]
+
+app_name = "cinema"
