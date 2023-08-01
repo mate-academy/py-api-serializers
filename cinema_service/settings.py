f72d8b1e3c523d505e9461d9a9c9010b55dce748
Nazariy <mac@Nazariys-MacBook-Pro.local>
2023-08-01 12:40:07 +0300

    fixed DebugToolbar

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -14,6 +14,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -23,6 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    "debug_toolbar",
     "rest_framework",
 
     "cinema",
@@ -37,6 +42,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+    "debug_toolbar.middleware.DebugToolbarMiddleware"
 ]
 
 ROOT_URLCONF = "cinema_service.urls"
