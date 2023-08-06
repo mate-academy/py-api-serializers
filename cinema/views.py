d4e96fd4edbd3811f082cf67e7ee6b2ac1fab73f
Vlad Dukh <vladdukh15@gmail.com>
2023-08-06 15:14:26 +0300

    'Solution'

--- cinema/views.py
+++ cinema/views.py
@@ -30,7 +30,7 @@ class CinemaHallViewSet(viewsets.ModelViewSet):
 
 
 class MovieViewSet(viewsets.ModelViewSet):
-    queryset = Movie.objects.all().prefetch_related("genres", "actors")
+    queryset = Movie.objects.prefetch_related("genres", "actors")
     serializer_class = MovieSerializer
 
     def get_serializer_class(self):
