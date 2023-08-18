efae3c265edd89d6f3fb96bc3585fa1671c02ce2
Taras Savchyn <savik1992@gmail.com>
2023-08-18 10:52:10 +0300

    add prefetch_related()

--- cinema/views.py
+++ cinema/views.py
@@ -52,7 +52,7 @@ class ActorViewSet(viewsets.ModelViewSet):
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
-    queryset = MovieSession.objects.all().select_related("movie", "cinema_hall")
+    queryset = MovieSession.objects.all()
     serializer_class = MovieSessionSerializer
 
     def get_serializer_class(self):
