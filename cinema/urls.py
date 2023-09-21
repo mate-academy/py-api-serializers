852d8a654d1529ed5636c32c3ef8737220c05ca3
Viacheslav Yasynetskyi <slawa1594@gmail.com>
2023-09-21 11:57:08 +0100

    add changes in urls.py

--- cinema/urls.py
+++ cinema/urls.py
@@ -19,8 +19,6 @@ router.register("movie_sessions", MovieSessionViewSet)
 router.register("orders", OrderViewSet)
 
 
-urlpatterns = [
-    path("", include(router.urls))
-]
+urlpatterns = router.urls
 
 app_name = "cinema"
