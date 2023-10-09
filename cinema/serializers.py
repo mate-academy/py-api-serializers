b6b745e5097b345f566be60e0e9572a47dbad1fb
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-09 19:40:17 +0300

    implement MovieSessionListSerializer and MovieSessionDetailSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -24,7 +24,7 @@ class ActorSerializer(serializers.ModelSerializer):
 class CinemaHallSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = CinemaHall
-		fields = "__all__"
+		fields = ("id", "name", "rows", "seats_in_row", "capacity")
 
 
 class MovieSerializer(serializers.ModelSerializer):
@@ -63,3 +63,22 @@ class MovieSessionSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = MovieSession
 		fields = "__all__"
+
+
+class MovieSessionListSerializer(MovieSessionSerializer):
+	movie_title = serializers.CharField(source="movie.title", read_only=True)
+	cinema_hall_name = serializers.CharField(source="cinema_hall.name", read_only=True)
+	cinema_hall_capacity = serializers.IntegerField(source="cinema_hall.capacity", read_only=True)
+
+	class Meta:
+		model = MovieSession
+		fields = ("id", "show_time", "movie_title", "cinema_hall_name", "cinema_hall_capacity")
+
+
+class MovieSessionDetailSerializer(serializers.ModelSerializer):
+	movie = MovieListSerializer(read_only=True)
+	cinema_hall = CinemaHallSerializer(read_only=True)
+
+	class Meta:
+		model = MovieSession
+		fields = ("id", "show_time", "movie", "cinema_hall")
