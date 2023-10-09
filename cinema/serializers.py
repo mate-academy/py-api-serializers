175cebedb8b7926f97d5a2bb54b7e19dc2d011d9
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-09 17:58:25 +0300

    initialized serializers for all models; implemented GenreSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,31 @@
-# write serializers here
+from rest_framework import serializers
+
+from cinema.models import (
+	Genre,
+	Actor,
+	CinemaHall,
+	Movie,
+	MovieSession
+)
+
+
+class GenreSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Genre
+		fields = "__all__"
+
+
+class ActorSerializer(serializers.ModelSerializer):
+	pass
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+	pass
+
+
+class MovieSerializer(serializers.ModelSerializer):
+	pass
+
+
+class MovieSessionSerializer(serializers.ModelSerializer):
+	pass
