00f9fa166ac462053dd520428ea8772b3cf7955c
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-09 17:59:18 +0300

    implemented GenreViewSet

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,21 @@
-# write views here
+from rest_framework import viewsets
+
+from cinema.models import (
+	Genre,
+	Actor,
+	CinemaHall,
+	Movie,
+	MovieSession
+)
+from cinema.serializers import (
+	GenreSerializer,
+	ActorSerializer,
+	CinemaHallSerializer,
+	MovieSerializer,
+	MovieSessionSerializer
+)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+	queryset = Genre.objects.all()
+	serializer_class = GenreSerializer
