fb5fc7b790087c4724a274efe1c14e7af0487c3a
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-09 19:40:52 +0300

    add path for debug toolbar

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/cinema/", include("cinema.urls"))
+    path("api/cinema/", include("cinema.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
