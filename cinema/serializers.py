616f2b5c8ed423fc33396de5e8186295a56e5956
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-10 17:52:55 +0300

    remove redundant meta class in MovieListSerializer and MovieDetailSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -35,18 +35,10 @@ class MovieListSerializer(MovieSerializer):
         many=True, read_only=True, slug_field="full_name"
     )
 
-    class Meta:
-        model = Movie
-        fields = ("id", "title", "description", "duration", "genres", "actors")
-
 
 class MovieDetailSerializer(MovieSerializer):
     genres = GenreSerializer(many=True, read_only=True)
     actors = ActorSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Movie
-        fields = ("id", "title", "description", "duration", "genres", "actors")
 
 
 class MovieSessionSerializer(serializers.ModelSerializer):
