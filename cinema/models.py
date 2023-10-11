af71b0a4d7f40939e809e7b88c2211e15946e267
Yaroslav Skrypchenko <yarik2300@gmail.com>
2023-10-11 21:22:03 +0300

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -27,6 +27,7 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
+    @property
     def full_name(self):
         return self.first_name + " " + self.last_name
 
