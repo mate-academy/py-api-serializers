abc83c05911c857b095e5d2ceb1a24503e4b1e47
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-10-11 16:07:57 +0300

    Implement serializers and views for cinema project

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,61 @@
-# write views here
+from rest_framework import viewsets
+
+from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+from cinema.serializers import (
+    GenreSerializer,
+    ActorSerializer,
+    CinemaHallSerializer,
+    MovieDetailSerializer,
+    MovieSessionListSerializer,
+    MovieListSerializer,
+    MovieSessionDetailSerializer,
+    MovieSerializer,
+    MovieSessionSerializer,
+)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class CinemaHallViewSet(viewsets.ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.prefetch_related("genres", "actors")
+    serializer_class = MovieSerializer
+
+    def get_serializer_class(
+        self,
+    ) -> MovieSerializer | MovieListSerializer | MovieDetailSerializer:
+        if self.action == "list":
+            return MovieListSerializer
+        if self.action == "retrieve":
+            return MovieDetailSerializer
+        return MovieSerializer
+
+
+class MovieSessionViewSet(viewsets.ModelViewSet):
+    queryset = MovieSession.objects.prefetch_related("cinema_hall", "movie")
+    serializer_class = MovieSessionSerializer
+
+    def get_serializer_class(
+        self,
+    ) -> (
+        MovieSessionSerializer
+        | MovieSessionListSerializer
+        | MovieSessionDetailSerializer
+    ):
+        if self.action == "list":
+            return MovieSessionListSerializer
+        if self.action == "retrieve":
+            return MovieSessionDetailSerializer
+        return MovieSessionSerializer
