e62604c57965e30135650137ce751565ad9209e9
Yaroslav Mazurkevych <77452871+yaroslav-mazurkevych@users.noreply.github.com>
2023-10-17 14:50:24 +0300

    Update views.py

--- cinema/views.py
+++ cinema/views.py
@@ -46,7 +46,7 @@ class MovieViewSet(viewsets.ModelViewSet):
         if self.action == "retrieve":
             return MovieDetailSerializer
 
-        return MovieSerializer
+        return self.serializer_class
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
@@ -60,4 +60,4 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         if self.action == "retrieve":
             return MovieSessionDetailSerializer
 
-        return MovieSessionSerializer
+        return self.serializer_class
