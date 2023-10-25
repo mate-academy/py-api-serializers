29aa8aa4b65a72494bd03cac91809292beeaf3a1
Mykola <nickkozlov90@gmail.com>
2023-10-25 22:36:51 +0300

    Create CinemaHall serializer and viewset

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,15 @@
-# write serializers here
+from rest_framework import serializers
+
+from cinema.models import CinemaHall
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CinemaHall
+        fields = (
+            "name",
+            "rows",
+            "seats_in_row",
+            "capacity",
+        )
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,12 @@
-# write urls here
+from django.urls import path, include
+from rest_framework import routers
+
+from cinema.views import CinemaHallViewSet
+
+router = routers.DefaultRouter()
+router.register("cinema_halls", CinemaHallViewSet)
+urlpatterns = [
+    path("", include(router.urls))
+]
+
+app_name = "cinema"
