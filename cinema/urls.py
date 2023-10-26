a254238653cc97be10299b229beb257209afb546
Mykola <nickkozlov90@gmail.com>
2023-10-26 16:10:19 +0300

    Create all views

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,12 +1,19 @@
-from django.urls import path, include
 from rest_framework import routers
 
-from cinema.views import CinemaHallViewSet
+from cinema.views import (
+    CinemaHallViewSet,
+    GenreViewSet,
+    ActorViewSet,
+    MovieViewSet,
+    MovieSessionViewSet,
+)
 
 router = routers.DefaultRouter()
 router.register("cinema_halls", CinemaHallViewSet)
-urlpatterns = [
-    path("", include(router.urls))
-]
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("movies", MovieViewSet)
+router.register("movie_sessions", MovieSessionViewSet)
+urlpatterns = router.urls
 
 app_name = "cinema"
