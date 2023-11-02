262f605cfb1cef16ac8056f0e77cbcdb93f5cf60
Vlad Larin <lordgadyka71@gmail.com>
2023-11-02 19:04:16 +0200

    Implement CRUD for cinema models without customization

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,33 @@
-# write serializers here
+from rest_framework import serializers
+
+from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = "__all__"
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = "__all__"
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = "__all__"
+
+
+class MovieSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovieSession
+        fields = "__all__"
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,15 @@
-# write urls here
+from rest_framework import routers
+
+from cinema.views import GenreViewSet, ActorViewSet, CinemaHallViewSet, \
+    MovieViewSet, MovieSessionViewSet
+
+router = routers.DefaultRouter()
+router.register("genre", GenreViewSet)
+router.register("actor", ActorViewSet)
+router.register("cinema_hall", CinemaHallViewSet)
+router.register("movie", MovieViewSet)
+router.register("movie_session", MovieSessionViewSet)
+
+urlpatterns = [] + router.urls
+
+app_name = "cinema"
