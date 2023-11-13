fbae09650c6877f2043e3fcb9a34849d93ce1b32
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-11-13 11:56:14 +0200

    Create views for model

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,45 @@
-# write views here
+from rest_framework import viewsets
+
+from cinema.models import (Genre,
+                           Movie,
+                           Actor,
+                           CinemaHall,
+                           MovieSession
+                           )
+
+from cinema.serializers import (CinemaHallSerializer,
+                                ActorSerializer,
+                                GenreSerializer,
+                                MovieSerializer,
+                                MovieSessionSerializer
+                                )
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class CinemaHallViewSet(viewsets.ModelViewSet):
+
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class MovieSessionViewSet(viewsets.ModelViewSet):
+
+    queryset = MovieSession.objects.all()
+    serializer_class = MovieSessionSerializer
