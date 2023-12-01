af1d734716453a32343d98fc2341af078fb9f9dd
sakhaline <alexandra.ivva@gmail.com>
2023-12-01 20:47:49 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,59 @@
-# write views here
+from rest_framework import viewsets
+
+from cinema.models import (Genre,
+                           Actor,
+                           CinemaHall,
+                           MovieSession,
+                           Movie)
+
+from cinema.serializers import (GenreSerializer,
+                                ActorSerializer,
+                                CinemaHallSerializer,
+                                MovieSessionSerializer,
+                                MovieSessionListSerializer,
+                                MovieSessionRetrieveSerializer,
+                                MovieSerializer,
+                                MovieListSerializer,
+                                MovieRetrieveSerializer)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class CinemaHallViewSet(viewsets.ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
+
+
+class MovieSessionViewSet(viewsets.ModelViewSet):
+    queryset = MovieSession.objects.select_related(
+        "movie", "cinema_hall"
+    )
+    serializer_class = MovieSessionSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieSessionListSerializer
+        if self.action == "retrieve":
+            return MovieSessionRetrieveSerializer
+
+        return MovieSessionSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.prefetch_related("genres", "actors").all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieListSerializer
+        if self.action == "retrieve":
+            return MovieRetrieveSerializer
+
+        return MovieSerializer
