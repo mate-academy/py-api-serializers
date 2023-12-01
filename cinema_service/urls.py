16ddbc08c6191c4c095c32d1f4f067216c60091f
Denis Komandyr <sabsdaid@gmail.com>
2023-12-01 03:00:24 +0200

    Nice understanding, Awesome implementation

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path("api/cinema/", include("cinema.urls", namespace="cinema")),
     path("admin/", admin.site.urls),
 ]
