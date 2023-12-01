f1bf91975f09d2c86b03e38c7330f46329bca74f
Yaroslav Lysiuk <quiet.klirik@gmail.com>
2023-12-01 14:00:33 +0200

    Changes

--- cinema/urls.py
+++ cinema/urls.py
@@ -16,8 +16,6 @@ router.register("cinema_halls", CinemaHallViewSet)
 router.register("movies", MovieViewSet)
 router.register("movie_sessions", MovieSessionViewSet)
 
-urlpatterns = [
-    path("", include(router.urls)),
-]
+urlpatterns = router.urls
 
 app_name = "cinema"
