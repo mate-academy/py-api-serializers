78423b280d5aed99e8c6bd3647941f21dc5491e0
Denys <den.prislipskyi@gmail.com>
2023-12-02 00:04:06 +0100

    solution

--- cinema/models.py
+++ cinema/models.py
@@ -30,6 +30,9 @@ class Actor(models.Model):
     def __str__(self):
         return self.first_name + " " + self.last_name
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
