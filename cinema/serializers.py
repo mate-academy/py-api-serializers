4b741b472877f1c80b9b99f22580d83bd287ef2e
Viktor Deyneka <3lobites@gmail.com>
2024-01-30 22:22:19 +0200

    Model worked out Actor

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 
-from cinema.models import Genre
+from cinema.models import Genre, Actor
 
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = ("id", "name", )
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name")
 
--- cinema/views.py
+++ cinema/views.py
@@ -1,11 +1,15 @@
 from rest_framework import generics, viewsets
 
-from cinema.models import Genre
+from cinema.models import Genre, Actor
 
-from cinema.serializers import GenreSerializer
+from cinema.serializers import GenreSerializer, ActorSerializer
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
