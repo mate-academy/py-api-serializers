82a55a019b7974712adb15dccc0b000e792ada5b
Viktor Deyneka <3lobites@gmail.com>
2024-01-30 22:01:59 +0200

    Presets add gitignore *.sqlite3; edit settings TIME_ZONE to Europe/Kiev. Model worked out Genres edit: serialize, views, cinema\urls, cinema_service\urls

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,10 @@
-# write serializers here
+from rest_framework import serializers
+
+from cinema.models import Genre
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("id", "name", )
+
--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,11 @@
-# write views here
+from rest_framework import generics, viewsets
+
+from cinema.models import Genre
+
+from cinema.serializers import GenreSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
