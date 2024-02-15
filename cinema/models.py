88203e8febe59b4bc641ceb20f3c6aef9519f522
Ruslan Yanyshyn <ruslan.yanyshyn@yahoo.com>
2024-02-15 16:05:13 -0600

    Solution for API serializers

--- cinema/models.py
+++ cinema/models.py
@@ -1,6 +1,6 @@
+from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.conf import settings
 
 
 class CinemaHall(models.Model):
@@ -29,6 +29,9 @@ class Actor(models.Model):
 
     @property
     def full_name(self):
+        return self.first_name + " " + self.last_name
+
+    def __str__(self):
         return self.first_name + " " + self.last_name
 
 
@@ -93,9 +96,9 @@ class Ticket(models.Model):
                 raise ValidationError(
                     {
                         ticket_attr_name: f"{ticket_attr_name} number "
-                        f"must be in available range: "
-                        f"(1, {cinema_hall_attr_name}): "
-                        f"(1, {count_attrs})"
+                                          f"must be in available range: "
+                                          f"(1, {cinema_hall_attr_name}): "
+                                          f"(1, {count_attrs})"
                     }
                 )
 
