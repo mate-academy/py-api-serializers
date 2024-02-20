ef218bd9a4924e7b9fbb7199b440a491667ff744
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-02-20 19:07:08 +0200

    feat: add first realization of Cinema API

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,24 @@
-# write urls here
+from rest_framework.routers import DefaultRouter
+
+from django.urls import path, include
+
+from cinema.views import (
+    MovieViewSet,
+    ActorViewSet,
+    GenreViewSet,
+    CinemaHallViewSet,
+    MovieSessionViewSet
+)
+
+router = DefaultRouter()
+router.register("movies", MovieViewSet)
+router.register("actors", ActorViewSet)
+router.register("genres", GenreViewSet)
+router.register("cinema_halls", CinemaHallViewSet)
+router.register("movie_sessions", MovieSessionViewSet)
+
+urlpatterns = [
+    path("", include(router.urls))
+]
+
+app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,35 @@
-# write views here
+from rest_framework import viewsets
+
+from cinema.models import Movie, Actor, Genre, CinemaHall, MovieSession
+from cinema.serializers import (
+    MovieSerializer,
+    ActorSerializer,
+    GenreSerializer,
+    CinemaHallSerializer,
+    MovieSessionSerializer
+)
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class CinemaHallViewSet(viewsets.ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
+
+
+class MovieSessionViewSet(viewsets.ModelViewSet):
+    queryset = MovieSession.objects.all()
+    serializer_class = MovieSessionSerializer
