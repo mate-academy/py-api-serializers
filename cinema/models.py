9c1787824c549d999f53a4f2f3887f3a480f8015
Даня Пономаренко <you@example.com>
2024-03-06 11:42:48 +0200

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -66,8 +66,10 @@ class Order(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
+
     def __str__(self):
         return str(self.created_at)
+
     class Meta:
         ordering = ["-created_at"]
 
