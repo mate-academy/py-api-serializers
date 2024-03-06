cbf3f41d6adc29210908e435605a3282407ab8c3
Даня Пономаренко <you@example.com>
2024-03-06 11:17:45 +0200

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -1,35 +1,29 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
-
-
 class CinemaHall(models.Model):
     name = models.CharField(max_length=255)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
-
     @property
     def capacity(self) -> int:
         return self.rows * self.seats_in_row
-
     def __str__(self):
         return self.name
-
-
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
-
     def __str__(self):
         return self.name
-
-
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
     def __str__(self):
         return self.first_name + " " + self.last_name
-
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
@@ -37,24 +31,19 @@ class Movie(models.Model):
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
-
     class Meta:
         ordering = ["title"]
-
     def __str__(self):
         return self.title
-
-
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
-
     class Meta:
         ordering = ["-show_time"]
 
     def __str__(self):
-        return self.movie.title + " " + str(self.show_time)
+        return str(self.movie.title) + " " + str(self.show_time)
 
 
 class Order(models.Model):
@@ -62,14 +51,10 @@ class Order(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
-
     def __str__(self):
         return str(self.created_at)
-
     class Meta:
         ordering = ["-created_at"]
-
-
 class Ticket(models.Model):
     movie_session = models.ForeignKey(
         MovieSession, on_delete=models.CASCADE, related_name="tickets"
@@ -79,7 +64,6 @@ class Ticket(models.Model):
     )
     row = models.IntegerField()
     seat = models.IntegerField()
-
     def clean(self):
         for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
             (self.row, "row", "count_rows"),
@@ -97,11 +81,9 @@ class Ticket(models.Model):
                         f"(1, {count_attrs})"
                     }
                 )
-
     def __str__(self):
         return (
             f"{str(self.movie_session)} (row: {self.row}, seat: {self.seat})"
         )
-
     class Meta:
         unique_together = ("movie_session", "row", "seat")
