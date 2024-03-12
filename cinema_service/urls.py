92ad5c99eb9a3f6e725bace47263b5eb311686a5
MaxymChyncha <max.chyncha@gmail.com>
2024-03-12 19:58:40 +0200

    refactor(cinema_service): modify url path for cinema app urls

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("cinema.urls", namespace="cinema")),
+    path("api/cinema/", include("cinema.urls", namespace="cinema")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
