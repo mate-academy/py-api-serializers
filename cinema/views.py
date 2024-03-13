818a059c6ec814848b332dc28147dcadec2abc3a
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-13 19:21:01 +0200

    Change queryset codestyle in views.py

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,5 @@
 from typing import Type
 
-from django.db.models import QuerySet
 from rest_framework import viewsets
 from rest_framework.serializers import Serializer
 
@@ -40,7 +39,7 @@ class CinemaHallViewSet(viewsets.ModelViewSet):
 
 
 class MovieViewSet(viewsets.ModelViewSet):
-    queryset = Movie.objects.all().prefetch_related("genres", "actors")
+    queryset = Movie.objects.prefetch_related("genres", "actors")
     serializer_class = MovieSerializer
 
     def get_serializer_class(self) -> Type[Serializer]:
@@ -54,7 +53,7 @@ class MovieViewSet(viewsets.ModelViewSet):
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
-    queryset = MovieSession.objects.all().select_related(
+    queryset = MovieSession.objects.select_related(
         "movie",
         "cinema_hall"
     )
