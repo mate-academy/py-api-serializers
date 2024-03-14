0eabe2e4b099da58ad11a695dea2701472af340b
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-03-14 19:16:01 +0200

    add genre, actor, cinema hall endpoints

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,30 @@
-# write serializers here
+from rest_framework import serializers
+
+from cinema.models import Genre, Actor, CinemaHall, Movie
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+
+class ActorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Actor
+        fields = "__all__"
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CinemaHall
+        fields = "__all__"
+
+
+class MovieSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Movie
+        fields = "__all__"
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,13 @@
-# write urls here
+from rest_framework import routers
+
+from cinema.views import GenreViewSet, ActorViewSet, CinemaHallViewSet
+
+router = routers.DefaultRouter()
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("cinema_halls", CinemaHallViewSet)
+
+
+urlpatterns = [] + router.urls
+
+app_name = "cinema"
