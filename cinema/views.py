a342224ab34c8250a7992c1fbf509023658c41a0
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-03-15 21:31:21 +0200

    refactor: Remove redundant method

--- cinema/views.py
+++ cinema/views.py
@@ -36,12 +36,8 @@ class CinemaHallViewSet(viewsets.ModelViewSet):
 
 
 class MovieViewSet(viewsets.ModelViewSet):
-    queryset = Movie.objects.all()
+    queryset = Movie.objects.prefetch_related("genres", "actors")
     serializer_class = MovieSerializer
-
-    def get_queryset(self):
-        queryset = self.queryset.prefetch_related("genres", "actors")
-        return queryset
 
     def get_serializer_class(self):
         if self.action == "retrieve":
@@ -54,12 +50,8 @@ class MovieViewSet(viewsets.ModelViewSet):
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
-    queryset = MovieSession.objects.all()
+    queryset = MovieSession.objects.prefetch_related("movie", "cinema_hall")
     serializer_class = MovieSessionSerializer
-
-    def get_queryset(self):
-        queryset = self.queryset.prefetch_related("movie", "cinema_hall")
-        return queryset
 
     def get_serializer_class(self):
         if self.action == "retrieve":
