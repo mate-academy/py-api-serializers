94290463dc1418935285dd070e15f4fa66bac01b
Serhii Dobrovinskii <serhiy.dobrovinskiy@gmail.com>
2024-03-18 13:54:39 +0200

    Sol

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -102,5 +102,5 @@ class MovieSessionListSerializer(serializers.ModelSerializer):
 
 class MovieSessionDetailSerializer(MovieSessionSerializer):
 
-    movie = MovieListSerializer(many=False, read_only=True)
-    cinema_hall = CinemaHallSerializer(many=False, read_only=True)
+    movie = MovieListSerializer(read_only=True)
+    cinema_hall = CinemaHallSerializer(read_only=True)
