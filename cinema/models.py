7888f2ad8f673940274aa97572f38133283e2d9c
Sikach Illia <sikatch.ilja@gmail.com>
2024-03-27 16:59:51 +0200

    1) use prefetch_related to actor 2) rewrite property to Actor model 3) fixed ActorSerializer

--- cinema/models.py
+++ cinema/models.py
@@ -27,6 +27,9 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.full_name
+
     @property
     def full_name(self):
         return self.first_name + " " + self.last_name
